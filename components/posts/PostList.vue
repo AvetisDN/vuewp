312f5d79e4520e70e1946666a060c3f857767c31
AvetisDN <avetisdn@gmail.com>
2019-07-14 11:53:13 +0300

    second commit

---
+++ components/posts/PostList.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul class="post-list">
+        <li v-for="post in posts" :key="post.id" class="post-list-item">
+            <PostExcerpt :post="post" />
+        </li>
+    </ul>
+</template>
+
+<script>
+    import PostExcerpt from './PostExcerpt.vue'
+
+    export default {
+        props: {
+            posts: { type: Array }
+        },
+        components: {
+            PostExcerpt
+        }
+    }
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,55 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'vuewp',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
     /*
-    ** Run ESLint on save
+    ** Headers of the page
     */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+    css: ['@/assets/css/style.scss'],
+    head: {
+        title: 'vuewp',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: 'Nuxt.js project'}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+        ],
+
+        script: [
+            {
+                src: "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+                type: "text/javascript"
+            },
+            {
+                src: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
+                type: "text/javascript"
+            },
+            {
+                src: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
+                type: "text/javascript"
+            }
+        ]
+    },
+    /*
+    ** Customize the progress bar color
+    */
+    loading: {color: '#e0452a'},
+    /*
+    ** Build configuration
+    */
+    build: {
+        /*
+        ** Run ESLint on save
+        */
+        extend(config, {isDev, isClient}) {
+            if (isDev && isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
     }
-  }
 }
 
